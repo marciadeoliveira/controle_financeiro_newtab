f6d3b66a08c047851e1f9fe1ce3e8f30af85668b
bimaggi <bmg.olivier@gmail.com>
2022-09-26 21:06:25 -0300

    js file-functions to add items to the extract, numeric and decimal value mask, updating values ​​and balance positive or negative

---
+++ src/dist/index.dev.js
@@ -0,0 +1,103 @@
+"use strict";
+
+var merchandise__value = document.querySelector('#merchandise__value');
+var transaction_type = document.querySelector('#transaction_type');
+var merchandise__type = document.querySelector('#merchandise__type');
+var extract = [];
+var sale = [];
+var buy = [];
+var result = 0;
+
+var createExtract = function createExtract(transaction_type, merchandise__type, merchandise__value) {
+  /* criar o elemento div, adicionar uma classe à ele, criar o que será mostrado no html 
+     e por fim criar os elementos.*/
+  var item = document.createElement('div');
+  item.classList.add('table__merchandise');
+  item.innerHTML = "\n    <div id=\"table__merchandise__operador\">".concat(transaction_type, "</div>\n    <div>").concat(merchandise__type, "</div>\n    <div class=\"header__value\">").concat(merchandise__value, "</div>\n   ");
+  document.querySelector('#nada').appendChild(item);
+}; // Mascara para validar o input de valor, restringindo o uso de letras e adicionando casas decimais
+
+
+var validNamber = function validNamber(e) {
+  e.preventDefault();
+  merchandise__value.value = merchandise__value.value.replace(/[^0-9]+/g, '');
+
+  if (merchandise__value.value.length <= 2) {
+    merchandise__value.value = ('000' + merchandise__value.value).substring(-3);
+  }
+
+  merchandise__value.value = merchandise__value.value.replace(/([0-9]{2})$/g, ".$1");
+  merchandise__value.value = parseFloat(merchandise__value.value).toLocaleString();
+};
+
+document.querySelector('#merchandise__value').addEventListener('keyup', validNamber, false); // para não repetir os mesmos valores quando atualizar a página
+
+var clearExtract = function clearExtract() {
+  var clear = document.querySelector('#nada');
+
+  while (clear.firstChild) {
+    clear.removeChild(clear.lastChild);
+  }
+}; // Renderiza se o financeiro gerou Lucro ou Prejuízo
+
+
+var renderBalance = function renderBalance(result) {
+  var balance = '';
+
+  if (result === 0) {
+    balance = '';
+  }
+
+  if (result > 0) {
+    balance = 'Lucro';
+  }
+
+  if (result < 0) {
+    balance = 'Prejuízo';
+  }
+
+  document.querySelector('#table__result').innerHTML = '[' + balance + ']';
+}; // Renderizar os reslutados de soma e subtração, chegando ao valor total do extrato
+
+
+var renderResult = function renderResult() {
+  var sum = sale.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+  }, 0);
+  var sub = buy.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+  }, 0);
+  result = sum - sub;
+  document.querySelector('#header__value').innerHTML = 'R$' + result;
+  renderBalance(result);
+}; // Renderizar o extrato, gerando tantos quantos elementos(div), estiverem dentro da const Extratotiverem
+
+
+var renderExtract = function renderExtract() {
+  clearExtract();
+  extract.forEach(function (item) {
+    return createExtract(item.type, item.merchandise, item.price);
+  });
+  renderResult();
+}; // Cadastrar os dados colhidos nos inputs 
+
+
+var addTransaction = function addTransaction(e) {
+  e.preventDefault();
+
+  if (transaction_type.value === "+") {
+    sale.push(+merchandise__value.value);
+  } else {
+    buy.push(+merchandise__value.value);
+  }
+
+  extract.push({
+    type: transaction_type.value,
+    merchandise: merchandise__type.value,
+    price: merchandise__value.value
+  });
+  renderExtract();
+};
+
+document.querySelector('#add_transaction').addEventListener('click', addTransaction, false);
+renderExtract();
---
+++ src/index.js
@@ -0,0 +1,92 @@
+const merchandise__value = document.querySelector('#merchandise__value')
+const transaction_type = document.querySelector('#transaction_type')
+const merchandise__type = document.querySelector('#merchandise__type')
+const extract = []
+let sale = []
+let buy = []
+let result = 0
+
+const createExtract =(transaction_type, merchandise__type, merchandise__value) => {
+/* criar o elemento div, adicionar uma classe à ele, criar o que será mostrado no html 
+   e por fim criar os elementos.*/
+  const item = document.createElement('div')
+  item.classList.add('table__merchandise')
+  item.innerHTML =`
+    <div id="table__merchandise__operador">${transaction_type}</div>
+    <div>${merchandise__type}</div>
+    <div class="header__value">${merchandise__value}</div>
+   `
+   document.querySelector('#nada').appendChild(item)
+}
+
+// Mascara para validar o input de valor, restringindo o uso de letras e adicionando casas decimais
+const validNamber =  ( e ) => {
+    e.preventDefault()
+    merchandise__value.value = merchandise__value.value.replace(/[^0-9]+/g,'')
+
+    if(merchandise__value.value.length <= 2) {
+      merchandise__value.value = ('000'+merchandise__value.value).substring(-3)
+    }
+    merchandise__value.value = merchandise__value.value.replace(/([0-9]{2})$/g,".$1")
+    merchandise__value.value = parseFloat(merchandise__value.value).toLocaleString()
+}
+document.querySelector('#merchandise__value').addEventListener('keyup', validNamber, false)
+
+// para não repetir os mesmos valores quando atualizar a página
+const clearExtract = () => {
+  const clear = document.querySelector('#nada')
+  while(clear.firstChild) {
+    clear.removeChild(clear.lastChild)
+  }
+}
+
+// Renderiza se o financeiro gerou Lucro ou Prejuízo
+const renderBalance = (result) => {
+  var balance =''
+  if( result === 0 ){
+    balance =''
+  } if( result > 0 ){
+    balance = 'Lucro' 
+  }if( result < 0) {
+    balance = 'Prejuízo' 
+  }
+  document.querySelector('#table__result').innerHTML = '['+ balance +']'
+} 
+// Renderizar os reslutados de soma e subtração, chegando ao valor total do extrato
+const renderResult = () => {
+  const sum = sale.reduce((previousValue, currentValue) => previousValue + currentValue,0)
+  const sub = buy.reduce((previousValue, currentValue) => previousValue + currentValue,0)
+  result = sum - sub
+  document.querySelector('#header__value').innerHTML = 'R$'+ result
+  renderBalance(result)
+}
+
+// Renderizar o extrato, gerando tantos quantos elementos(div), estiverem dentro da const Extratotiverem
+const renderExtract = () => {
+  clearExtract() 
+  extract.forEach(item => createExtract(item.type,item.merchandise, item.price))
+  renderResult()  
+ }
+
+// Cadastrar os dados colhidos nos inputs 
+const addTransaction = ( e ) => {
+  e.preventDefault()
+  if(transaction_type.value === "+") {
+    sale.push((+merchandise__value.value))
+  } else{
+    buy.push((+merchandise__value.value))
+  }
+  extract.push(
+    {
+      type: transaction_type.value,
+      merchandise: merchandise__type.value,
+      price: merchandise__value.value
+    }
+  )
+  renderExtract()
+}
+document.querySelector('#add_transaction').addEventListener('click',addTransaction, false)
+
+renderExtract()
+
+
