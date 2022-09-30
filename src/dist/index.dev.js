9f6e3b79b6aa9479fe633e580b9c20cde7c37728
bimaggi <bmg.olivier@gmail.com>
2022-09-30 11:16:46 -0300

    restructuring of functions to correct a bug when add the first data

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,9 +1,109 @@
 "use strict";
 
+var merchandise__value = document.querySelector('#merchandise__value');
+var transaction_type = document.querySelector('#transaction_type');
+var merchandise__type = document.querySelector('#merchandise__type');
+var extract = [];
+var sale;
+var buy;
+var balance;
+var result = 0; // pegar valores no localStorage
+
+localStorage.getItem('extract') != null ? extract = JSON.parse(localStorage.getItem('extract')) : '';
+localStorage.getItem('result') != null ? result = JSON.parse(localStorage.getItem('result')) : ''; //limpar os campos dos inputs após clicar no botão adicionar Transação
+
+var clearFields = function clearFields() {
+  document.querySelector('#transaction_type').value = '';
+  document.querySelector('#merchandise__value').value = '';
+  document.querySelector('#merchandise__type').value = '';
+}; //Renderiza se o financeiro gerou Lucro ou Prejuízo
+
+
+var renderBalance = function renderBalance() {
+  result > 0 ? balance = '[Lucro]' : balance = '[Prejuízo]';
+  localStorage.setItem('balance', JSON.stringify(balance));
+  document.querySelector('#table__result').innerHTML = balance;
+  clearFields();
+};
+/*criar o elemento div, adicionar uma classe à ele, 
+criar o que será mostrado no html e por fim criar os elementos.*/
+
+
+var createExtract = function createExtract(transaction_type, merchandise__type, merchandise__value) {
+  document.querySelector('#table__message').style.display = "none";
+  document.querySelector('#table').style.display = "block";
+  var item = document.createElement('div');
+  item.classList.add('table__merchandise');
+  item.innerHTML = "\n    <div id=\"table__merchandise__operador\">\n      ".concat(transaction_type, "\n    </div>\n    <div>\n      ").concat(merchandise__type, "\n    </div>\n    <div class=\"header__value\" id=\"header__value\">\n      ").concat(merchandise__value, "\n    </div>\n    ");
+  document.querySelector('#table__section').appendChild(item);
+  renderBalance();
+}; //Percorrer o array extrato e mandar os itens, para o createExctract, gerar os elementos HTML
+
+
+var renderExtract = function renderExtract() {
+  clearExtract();
+  extract.map(function (item) {
+    createExtract(item.type, item.merchandise, item.price);
+  });
+}; // para não repetir os mesmos valores já listados quando atualizar a página
+
+
+var clearExtract = function clearExtract() {
+  var clear = document.querySelector('#table__section');
+
+  while (clear.firstChild) {
+    clear.removeChild(clear.lastChild);
+  }
+};
+/*Mascara para validar o input de valor, restringindo o 
+uso de letras e adicionando casas decimais*/
+
+
+var validNamber = function validNamber(e) {
+  e.preventDefault();
+  merchandise__value.value = merchandise__value.value.replace(/[^0-9]+/g, '');
+
+  if (merchandise__value.value.length <= 2) {
+    merchandise__value.value = ('000' + merchandise__value.value).substring(-3);
+  }
+
+  merchandise__value.value = merchandise__value.value.replace(/([0-9]{2})$/g, ".$1");
+  merchandise__value.value = parseFloat(merchandise__value.value).toLocaleString();
+};
+
+document.querySelector('#merchandise__value').addEventListener('keyup', validNamber, false);
+/*Pegar os dados dos inputs adicionar nas variaveis e mandá-las para localStorage*/
+
+var addTransaction = function addTransaction(e) {
+  e.preventDefault();
+  if (!merchandise__type.value || !merchandise__value.value || !transaction_type.value) alert('Preencha os campos corretamente');
+  extract.push({
+    type: transaction_type.value,
+    merchandise: merchandise__type.value,
+    price: +merchandise__value.value
+  });
+
+  if (transaction_type.value === "+") {
+    sale = +merchandise__value.value;
+    result += sale;
+    document.querySelector('#footer__value').innerHTML = 'R$' + result;
+  } else {
+    buy = +merchandise__value.value;
+    result -= buy;
+    document.querySelector('#footer__value').innerHTML = 'R$' + result;
+  }
+
+  localStorage.setItem('result', JSON.stringify(result));
+  localStorage.setItem('extract', JSON.stringify(extract));
+  renderExtract();
+};
+
+document.querySelector('#add_transaction').addEventListener('click', addTransaction, false);
 /*limpar todos os dados ao clicar no campo limpar dados, 
-  exibir mensagem de confirmação e recarregar a pagina*/
+ exibir mensagem de confirmação e recarregar a pagina*/
+
 var clearData = function clearData() {
-  if (window.confirm("Você realmente deseja limpar todos os dados?")) {
+  if (window.confirm("Você deseja limpar todos os dados?")) {
     localStorage.clear('extract', extract);
     location.reload();
   }
@@ -22,124 +122,20 @@ var openMenu = function openMenu() {
   document.querySelector('#nav__menu').style.display = 'flex';
 };
 
-document.querySelector('#nav__collapse').addEventListener('click', openMenu, false); // pegar valores no localStorage
-
-localStorage.getItem('extract') != null ? extract = JSON.parse(localStorage.getItem('extract')) : '';
-localStorage.getItem('result') != null ? result = JSON.parse(localStorage.getItem('result')) : '';
+document.querySelector('#nav__collapse').addEventListener('click', openMenu, false);
+/*Verifica se o local storage tem dados para serem recarregados, quando 
+carregar a página*/
 
 var uploadDatas = function uploadDatas() {
-  var merchandise__value = document.querySelector('#merchandise__value');
-  var transaction_type = document.querySelector('#transaction_type');
-  var merchandise__type = document.querySelector('#merchandise__type');
-  var extract = [];
-  var sale;
-  var buy;
-  var balance; // não posso criar a variavel result aqui pq senão quebra tudo, afinal ela já está sendo atribuida ao chamar o get Item lá no começo do código
-  //let result=0
-
-  localStorage.getItem('result') == null ? result = JSON.parse(localStorage.getItem('result')) : ''; //limpar os campos dos inputs após clicar no botão adicionar Transação
-
-  var clearFields = function clearFields() {
-    document.querySelector('#transaction_type').value = '';
-    document.querySelector('#merchandise__value').value = '';
-    document.querySelector('#merchandise__type').value = '';
-  }; //Renderiza se o financeiro gerou Lucro ou Prejuízo
-
-
-  var renderBalance = function renderBalance() {
-    localStorage.getItem(result);
-    result > 0 ? balance = '[Lucro]' : balance = '[Prejuízo]';
-    localStorage.setItem('balance', JSON.stringify(balance));
-    document.querySelector('#table__result').innerHTML = balance;
-    clearFields();
-  };
-  /*criar o elemento div, adicionar uma classe à ele, 
-  criar o que será mostrado no html e por fim criar os elementos.*/
-
-
-  var createExtract = function createExtract(transaction_type, merchandise__type, merchandise__value) {
-    //document.querySelector('#table__message').style.display = "none"
-    //document.querySelector('#table').style.display = "block"
-    var item = document.createElement('div');
-    item.classList.add('table__merchandise');
-    item.innerHTML = "\n      <div id=\"table__merchandise__operador\">\n        ".concat(transaction_type, "\n      </div>\n      <div>\n        ").concat(merchandise__type, "\n      </div>\n      <div class=\"header__value\" id=\"header__value\">\n        ").concat(merchandise__value, "\n      </div>\n      ");
-    document.querySelector('#table__section').appendChild(item);
-    renderBalance();
-  }; //Percorrer o array extrato e mandar 
-
-
-  var renderExtract = function renderExtract() {
-    clearExtract();
-    extract.map(function (item) {
-      createExtract(item.type, item.merchandise, item.price);
-    });
-  }; // para não repetir os mesmos valores quando atualizar a página
-
-
-  var clearExtract = function clearExtract() {
-    var clear = document.querySelector('#table__section');
-
-    while (clear.firstChild) {
-      clear.removeChild(clear.lastChild);
-    }
-  };
-
-  var addTransaction = function addTransaction(e) {
-    e.preventDefault();
-    if (!merchandise__type.value || !merchandise__value.value || !transaction_type.value) alert('Preencha os campos corretamente');
-    extract.push({
-      type: transaction_type.value,
-      merchandise: merchandise__type.value,
-      price: +merchandise__value.value
-    });
-    localStorage.getItem(result);
-
-    if (transaction_type.value === "+") {
-      sale = +merchandise__value.value;
-      result += sale;
-      document.querySelector('#footer__value').innerHTML = 'R$' + result;
-    } else {
-      buy = +merchandise__value.value;
-      result -= buy;
-      document.querySelector('#footer__value').innerHTML = 'R$' + result;
-    }
-
-    localStorage.setItem('result', JSON.stringify(result));
-    localStorage.setItem('extract', JSON.stringify(extract));
-    renderExtract();
-  };
-
-  document.querySelector('#add_transaction').addEventListener('click', addTransaction, false);
-  /*Mascara para validar o input de valor, restringindo o 
-  uso de letras e adicionando casas decimais*/
-
-  var validNamber = function validNamber(e) {
-    e.preventDefault();
-    merchandise__value.value = merchandise__value.value.replace(/[^0-9]+/g, '');
-
-    if (merchandise__value.value.length <= 2) {
-      merchandise__value.value = ('000' + merchandise__value.value).substring(-3);
-    }
-
-    merchandise__value.value = merchandise__value.value.replace(/([0-9]{2})$/g, ".$1");
-    merchandise__value.value = parseFloat(merchandise__value.value).toLocaleString();
-  };
-
-  document.querySelector('#merchandise__value').addEventListener('keyup', validNamber, false);
-  /*criar o elemento div, adicionar uma classe à ele, 
-  criar o que será mostrado no html e por fim criar os elementos.*/
-
   if (localStorage.length == 0) {
     document.querySelector('#table__message').style.display = "flex";
     document.querySelector('#table').style.display = "none";
   } else {
-    localStorage.getItem(result);
     document.querySelector('#table__message').style.display = "none";
     document.querySelector('#table').style.display = "block";
     document.querySelector('#footer__value').innerHTML = 'R$' + result;
     localStorage.getItem(balance);
     document.querySelector('#table__result').innerHTML = balance;
-    extract = JSON.parse(localStorage.getItem('extract'));
     extract.map(function (item) {
       createExtract(item.type, item.merchandise, item.price);
     });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,135 @@
+const merchandise__value = document.querySelector('#merchandise__value')
+const transaction_type = document.querySelector('#transaction_type')
+const merchandise__type = document.querySelector('#merchandise__type')
+let extract = []
+let sale;
+let buy;
+let balance;
+let result= 0
+
+// pegar valores no localStorage
+localStorage.getItem('extract') != null 
+? extract = JSON.parse(localStorage.getItem('extract'))
+:''
+
+localStorage.getItem('result') != null 
+? result = JSON.parse(localStorage.getItem('result'))
+:''
+
+//limpar os campos dos inputs após clicar no botão adicionar Transação
+const clearFields = () => {
+  document.querySelector('#transaction_type').value = ''
+  document.querySelector('#merchandise__value').value = ''
+  document.querySelector('#merchandise__type').value = ''
+}
+
+//Renderiza se o financeiro gerou Lucro ou Prejuízo
+const renderBalance = ( )=> {
+  result > 0 ? balance ='[Lucro]': balance = '[Prejuízo]'
+  localStorage.setItem('balance', JSON.stringify(balance));
+  document.querySelector('#table__result').innerHTML = balance 
+  clearFields()
+} 
+
+ /*criar o elemento div, adicionar uma classe à ele, 
+criar o que será mostrado no html e por fim criar os elementos.*/
+const createExtract = (
+  transaction_type, 
+  merchandise__type, 
+  merchandise__value
+) => {
+  document.querySelector('#table__message').style.display = "none"
+  document.querySelector('#table').style.display = "block"
+
+  const item = document.createElement('div')
+  item.classList.add('table__merchandise')
+  item.innerHTML =`
+    <div id="table__merchandise__operador">
+      ${transaction_type}
+    </div>
+    <div>
+      ${merchandise__type}
+    </div>
+    <div class="header__value" id="header__value">
+      ${merchandise__value}
+    </div>
+    `
+  document.querySelector('#table__section').appendChild(item)
+  renderBalance()  
+}
+
+//Percorrer o array extrato e mandar os itens, para o createExctract, gerar os elementos HTML
+const renderExtract = () => {
+  clearExtract() 
+  extract.map(item => {
+    createExtract(item.type, item.merchandise, item.price)
+  })
+}
+   
+// para não repetir os mesmos valores já listados quando atualizar a página
+const clearExtract = () => {
+  const clear = document.querySelector('#table__section')
+  while(clear.firstChild) {
+    clear.removeChild(clear.lastChild)
+  }
+}
+
+/*Mascara para validar o input de valor, restringindo o 
+uso de letras e adicionando casas decimais*/
+const validNamber = (e) => {
+  e.preventDefault()
+  merchandise__value.value = merchandise__value.value
+  .replace(/[^0-9]+/g,'')
+
+  if(merchandise__value.value.length <= 2) {
+    merchandise__value.value = ('000'+merchandise__value.value)
+    .substring(-3)
+  }
+  merchandise__value.value = merchandise__value.value
+  .replace(/([0-9]{2})$/g,".$1")
+  merchandise__value.value = parseFloat(merchandise__value.value)
+  .toLocaleString()
+}
+document.querySelector('#merchandise__value')
+.addEventListener('keyup',validNamber,false)
+
+/*Pegar os dados dos inputs adicionar nas variaveis e mandá-las para localStorage*/
+const addTransaction = (e) => {
+  e.preventDefault()
+  if(
+    !merchandise__type.value 
+    || !merchandise__value.value 
+    || !transaction_type.value
+  ) 
+  alert('Preencha os campos corretamente')
+  extract.push(
+    { 
+      type: transaction_type.value, 
+      merchandise: merchandise__type.value,
+      price: +merchandise__value.value,
+    }
+  )
+  if(transaction_type.value === "+") {
+    sale = +merchandise__value.value
+    result += sale
+    document.querySelector('#footer__value').innerHTML = 'R$'+ result 
+  } else{
+    buy = +merchandise__value.value
+    result -= buy
+    document.querySelector('#footer__value').innerHTML = 'R$'+ result 
+  }
+  localStorage.setItem('result',JSON.stringify(result))
+  localStorage.setItem('extract',JSON.stringify(extract))
+  renderExtract()
+}
+document.querySelector('#add_transaction')
+.addEventListener('click',addTransaction,false)
+
+
 /*limpar todos os dados ao clicar no campo limpar dados, 
-  exibir mensagem de confirmação e recarregar a pagina*/
+ exibir mensagem de confirmação e recarregar a pagina*/
 const clearData = () => {
-  if(window.confirm("Você realmente deseja limpar todos os dados?")) {
+  if(window.confirm("Você deseja limpar todos os dados?")) {
     localStorage.clear('extract', extract)
     location. reload() 
   }
@@ -24,147 +152,13 @@ const openMenu = () => {
 document.querySelector('#nav__collapse')
 .addEventListener('click', openMenu, false)
 
-// pegar valores no localStorage
-localStorage.getItem('extract') != null 
-? extract = JSON.parse(localStorage.getItem('extract'))
-:''
-
-localStorage.getItem('result') != null 
-? result = JSON.parse(localStorage.getItem('result'))
-:''
-
+/*Verifica se o local storage tem dados para serem recarregados, quando 
+carregar a página*/
 const uploadDatas = () => {
-  const merchandise__value = document.querySelector('#merchandise__value')
-  const transaction_type = document.querySelector('#transaction_type')
-  const merchandise__type = document.querySelector('#merchandise__type')
-  let extract = []
-  let sale;
-  let buy;
-  let balance;
-   // não posso criar a variavel result aqui pq senão quebra tudo, afinal ela já está sendo atribuida ao chamar o get Item lá no começo do código
-  //let result=0
-  localStorage.getItem('result') == null 
-? result = JSON.parse(localStorage.getItem('result'))
-:''
-
-  //limpar os campos dos inputs após clicar no botão adicionar Transação
-  const clearFields = () => {
-    document.querySelector('#transaction_type').value = ''
-    document.querySelector('#merchandise__value').value = ''
-    document.querySelector('#merchandise__type').value = ''
-  }
-  //Renderiza se o financeiro gerou Lucro ou Prejuízo
-  const renderBalance = ( )=> {
-    localStorage.getItem(result)
-    result > 0 ? balance ='[Lucro]': balance = '[Prejuízo]'
-    localStorage.setItem('balance', JSON.stringify(balance));
-    document.querySelector('#table__result').innerHTML = balance 
-    clearFields()
-  } 
-  
-   /*criar o elemento div, adicionar uma classe à ele, 
-  criar o que será mostrado no html e por fim criar os elementos.*/
-  const createExtract = (
-    transaction_type, 
-    merchandise__type, 
-    merchandise__value
-  ) => {
-    //document.querySelector('#table__message').style.display = "none"
-    //document.querySelector('#table').style.display = "block"
- 
-    const item = document.createElement('div')
-    item.classList.add('table__merchandise')
-    item.innerHTML =`
-      <div id="table__merchandise__operador">
-        ${transaction_type}
-      </div>
-      <div>
-        ${merchandise__type}
-      </div>
-      <div class="header__value" id="header__value">
-        ${merchandise__value}
-      </div>
-      `
-    document.querySelector('#table__section').appendChild(item)
-    renderBalance()  
-}
-
-  //Percorrer o array extrato e mandar 
-  const renderExtract = () => {
-    clearExtract() 
-    extract.map(item => {
-      createExtract(item.type, item.merchandise, item.price)
-    })
-   }
-   
-  // para não repetir os mesmos valores quando atualizar a página
-  const clearExtract = () => {
-    const clear = document.querySelector('#table__section')
-    while(clear.firstChild) {
-      clear.removeChild(clear.lastChild)
-    }
-  }
-
-  const addTransaction = ( e ) => {
-    e.preventDefault()
-    if(
-      !merchandise__type.value 
-      || !merchandise__value.value 
-      || !transaction_type.value
-    ) 
-    alert('Preencha os campos corretamente')
-    extract.push(
-      { 
-        type: transaction_type.value, 
-        merchandise: merchandise__type.value,
-        price: +merchandise__value.value,
-      }
-    )
-    localStorage.getItem(result)
-    if(transaction_type.value === "+") {
-      sale = +merchandise__value.value
-      result += sale
-      document.querySelector('#footer__value').innerHTML = 'R$'+ result 
-    } else{
-      buy = +merchandise__value.value
-      result -= buy
-      document.querySelector('#footer__value').innerHTML = 'R$'+ result 
-    }
-    localStorage.setItem('result',JSON.stringify(result))
-    localStorage.setItem('extract',JSON.stringify(extract))
-    renderExtract()
-    }
-  document.querySelector('#add_transaction')
-  .addEventListener('click',addTransaction,false)
-
-
-  /*Mascara para validar o input de valor, restringindo o 
-  uso de letras e adicionando casas decimais*/
-  const validNamber = (e) => {
-    e.preventDefault()
-    merchandise__value.value = merchandise__value.value
-    .replace(/[^0-9]+/g,'')
-  
-    if(merchandise__value.value.length <= 2) {
-      merchandise__value.value = ('000'+merchandise__value.value)
-      .substring(-3)
-    }
-    merchandise__value.value = merchandise__value.value
-    .replace(/([0-9]{2})$/g,".$1")
-    merchandise__value.value = parseFloat(merchandise__value.value)
-    .toLocaleString()
-  }
-  document.querySelector('#merchandise__value')
-  .addEventListener('keyup',validNamber,false)
-
-  /*criar o elemento div, adicionar uma classe à ele, 
-  criar o que será mostrado no html e por fim criar os elementos.*/
-  
   if(localStorage.length == 0){
     document.querySelector('#table__message').style.display ="flex"
     document.querySelector('#table').style.display ="none"
   }else{
-    localStorage.getItem(result)
     document.querySelector('#table__message').style.display ="none"
     document.querySelector('#table').style.display ="block"
 
@@ -172,7 +166,6 @@ const uploadDatas = () => {
     localStorage.getItem(balance)
 
     document.querySelector('#table__result').innerHTML = balance 
-    extract = JSON.parse(localStorage.getItem('extract'))
 
     extract.map(item => {
       createExtract(item.type, item.merchandise, item.price)
@@ -180,27 +173,3 @@ const uploadDatas = () => {
   }
 }
 uploadDatas()
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
